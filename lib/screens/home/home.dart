e0ea512b173f91428aa80724a1e42403e750c45d
Tarius <tarius.business@gmail.com>
2021-11-21 17:15:48 +0700

    restructure

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:suvile/widgets/home_app_bar.dart';
+import 'package:suvile/screens/home/widgets/home_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
